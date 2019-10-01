855b491ff94f384ac5bf8c9e34d1f980db2fc1cf
Macarena <Macarena@MacBook-Pro-de-Macarena.local>
2019-09-30 21:52:48 -0300

    pantallas de perfil faltantes

--- home.php
+++ home.php
@@ -23,29 +23,29 @@
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="https://images.pexels.com/photos/2733663/pexels-photo-2733663.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block bg-light">
-            <a href="productos.php">
+          <div class="carousel-caption d-none d-md-block">
+            <!-- <a href="productos.php">
               <h5>Ir a ver celulares</h5>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </a>
+            </a> -->
           </div>
         </div>
         <div class="carousel-item">
           <img src="https://images.pexels.com/photos/196649/pexels-photo-196649.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block bg-light">
-            <a href="productos.php">
+          <div class="carousel-caption d-none d-md-block">
+            <!-- <a href="productos.php">
               <h5>Ir a ver tablets</h5>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </a>
+            </a> -->
           </div>
         </div>
         <div class="carousel-item">
           <img src="https://images.pexels.com/photos/461077/pexels-photo-461077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block bg-light">
-          <a href="productos.php">
+          <div class="carousel-caption d-none d-md-block">
+          <!-- <a href="productos.php">
             <h5>Ir a ver notebooks</h5>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </a>
+          </a> -->
           </div>
         </div>
       </div>
@@ -60,27 +60,29 @@
     </div>
   </div>
 
-  <div class="container marketing">
+  <div class="container marketing  mt-5">
 
     <!-- Three columns of text below the carousel -->
     <div class="row">
-      <div class="col-lg-4 mt-20">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-        <h2>Heading</h2>
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect fill="#777" width="100%" height="100%" fill="#"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+        <h2></h2>
         <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+        <p><a class="btn btn-secondary" href="#" role="button">Ver celulares</a></p>
       </div><!-- /.col-lg-4 -->
+
       <div class="col-lg-4">
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-        <h2>Heading</h2>
+        <h2></h2>
         <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+        <p><a class="btn btn-secondary" href="#" role="button">Ver notebooks</a></p>
       </div><!-- /.col-lg-4 -->
+
       <div class="col-lg-4">
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-        <h2>Heading</h2>
+        <h2></h2>
         <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+        <p><a class="btn btn-secondary" href="#" role="button">Ver tablets</a></p>
       </div><!-- /.col-lg-4 -->
     </div><!-- /.row -->
 
